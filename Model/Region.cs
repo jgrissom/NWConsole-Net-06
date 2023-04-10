4e21b11c84f47e42cf77cc819cf4c80c92b93353
jgrissom <jgrissom91@gmail.com>
2023-04-10 13:22:35 -0500

    [1.06] Scaffold database context and entity classes
    dotnet ef dbcontext scaffold "Server=bitsql.wctc.edu;Database=NWConsole_##_XXX;User ID=YYY;Password=ZZZ" Microsoft.EntityFrameworkCore.SqlServer -o Model
    -----------------------------------------------------------------------
    - IMPORTANT: remove connection string from database context
    - Use connection string stored in appsettings.json

---
+++ Model/Region.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Northwind_Console_Net06.Model
+{
+    public partial class Region
+    {
+        public Region()
+        {
+            Territories = new HashSet<Territory>();
+        }
+
+        public int RegionId { get; set; }
+        public string RegionDescription { get; set; }
+
+        public virtual ICollection<Territory> Territories { get; set; }
+    }
+}
